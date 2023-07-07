d455b1f48aaf1d8f920517f6d30fac7315dc123a
Tanya M <130961406+sciencehas@users.noreply.github.com>
2023-07-07 16:13:30 -0400

    Update apps.py

--- project_root/app/apps/document_processor/apps.py
+++ project_root/app/apps/document_processor/apps.py
@@ -1,7 +1,5 @@
-```python
 from django.apps import AppConfig
 
 class DocumentProcessorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'document_processor'
-```
