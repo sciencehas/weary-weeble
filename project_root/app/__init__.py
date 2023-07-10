896bd7bb08a4d3209b83833faefae0e2ce91eefe
Tanya M <130961406+sciencehas@users.noreply.github.com>
2023-07-09 21:22:02 -0400

    Update __init__.py

--- project_root/app/__init__.py
+++ project_root/app/__init__.py
@@ -1,5 +1,3 @@
 The __init__.py file is usually empty and is used to indicate that the directory it is in should be treated as a package. Here is the code for the __init__.py file:
 
-```python
 # Empty file to indicate this directory is a package
-```
