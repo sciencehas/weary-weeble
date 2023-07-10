04ceea9aa1aa578cb3f99c1c95fdabada6f98858
Tanya M <130961406+sciencehas@users.noreply.github.com>
2023-07-09 21:21:17 -0400

    Update wsgi.py

--- project_root/wsgi.py
+++ project_root/wsgi.py
@@ -1,4 +1,3 @@
-```python
 """
 WSGI config for project_root project.
 
@@ -15,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_root.settings')
 
 application = get_wsgi_application()
-```
